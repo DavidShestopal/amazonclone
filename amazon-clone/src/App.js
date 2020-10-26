798022d5833470b8a3881c22ecab45ffd93efdb5
David Shestopal <dshestopal@gmail.com>
2020-10-26 15:10:54 -0700

    checkout

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,13 +11,13 @@ function App() {
       <div className="app">
         <Header />
         <Switch>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>{' '}
-          <Route path="/">
+          <Route exact path="/">
             {' '}
             <Home />
           </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>{' '}
         </Switch>
       </div>
     </Router>
