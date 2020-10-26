a6fdfde459e65ced7dc994d89205dd05e01b06f1
David Shestopal <dshestopal@gmail.com>
2020-10-26 10:31:42 -0700

    firebase config added

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnR46XOKY3v1A25MydbPTGLmvcaBss2Zc',
+  authDomain: 'clone-db7b2.firebaseapp.com',
+  databaseURL: 'https://clone-db7b2.firebaseio.com',
+  projectId: 'clone-db7b2',
+  storageBucket: 'clone-db7b2.appspot.com',
+  messagingSenderId: '136770179177',
+  appId: '1:136770179177:web:b7a9c6f038089e258d52f8',
+  measurementId: 'G-7YRJR2YMN2',
+};
