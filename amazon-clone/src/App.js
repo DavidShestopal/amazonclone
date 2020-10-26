ab0bc710bb208a740c15b2d07b207997b6d4e2ec
David Shestopal <dshestopal@gmail.com>
2020-10-26 11:36:44 -0700

    header built

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
