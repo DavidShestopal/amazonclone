c4da95e3f4361307a73c23e5ab0ff8276b11f646
David Shestopal <dshestopal@gmail.com>
2020-10-27 16:12:06 -0700

    added subtotal and shopping cart count

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
-
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
-function header() {
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -20,7 +22,7 @@ function header() {
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
+          <span className="header__optionLineTwo">Sign in</span>
         </div>
 
         <div className="header__option">
@@ -32,11 +34,11 @@ function header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
@@ -44,4 +46,4 @@ function header() {
   );
 }
 
-export default header;
+export default Header;
