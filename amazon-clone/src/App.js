d5aed895211e949b739bad1313a16da7ddd71ac4
David Shestopal <dshestopal@gmail.com>
2020-10-28 18:40:13 -0700

    login created

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
-          <Route exact path="/">
-            {' '}
-            <Home />
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
-          </Route>{' '}
+          </Route>
+          <Route exact path="/">
+            <Header />
+            <Home />
+          </Route>
         </Switch>
       </div>
     </Router>
